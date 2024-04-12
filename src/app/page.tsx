3ec36332c907ec35a1928dcecb07777048ecc496
okoid721 <okoid721@gmail.com>
2024-04-12 16:14:27 +0100

    form builder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from '@/components/ui/button';
+import FormGenerator from './form-genarator/FormGenerator';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Create a form</Button>
+      <FormGenerator />
     </main>
   );
 }
